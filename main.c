3dca3aef892e7ed4c5c84ea1abb67875f9ee80a5
aleksgorovenko <aleksgorovenko@gmail.com>
2016-04-26 19:52:44 +0600

    Add headers and struct

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <getopt.h>
+#include <memory.h>
+
+typedef struct
+{
+	int input;
+	char input_file[255];
+	int output;
+	char output_file[255];
+	int alg;
+	char alg_name[255];
+	int shift;
+}	input_struct;
 
 int main(int argc, char* argv[])
 {
