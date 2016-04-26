a69b1742a69ab6afeb8c954423219cfad99b0fa4
aleksgorovenko <aleksgorovenko@gmail.com>
2016-04-26 19:55:15 +0600

    Processing command line arguments

--- main.c
+++ main.c
@@ -22,6 +22,44 @@ int main(int argc, char* argv[])
 	int option;
 	FILE* input_file = NULL;
 	FILE* output_file = NULL;
+	
+	data.shift = 0;
+	while((option = getopt_long(argc, argv, "i:o:a:s:", 0, 0)) != -1)
+	{
+		switch (option)
+		{
+			case 'i':
+				if ((data.input != 1) && (optarg[0] != '-') && (strlen(optarg) < 255))
+				{
+					data.input = 1;
+					memcpy(data.input_file, optarg, strlen(optarg) + 1);
+				}
+				break;
+			case 'o':
+				if ((data.output != 1)  && (optarg[0] != '-') && (strlen(optarg) < 255))
+				{
+					data.output = 1;
+					memcpy(data.output_file, optarg, strlen(optarg) + 1);
+				}
+				break;
+			case 'a':
+				if ((data.alg != 1) && (optarg[0] != '-') && (strlen(optarg) < 255))
+				{
+					data.alg = 1;
+					memcpy(data.alg_name, optarg, strlen(optarg) + 1);
+				}
+				break;
+			case 's':
+				if ((data.shift == 0) && (optarg[0] != '-') && (strlen(optarg) < 255))
+				{
+					data.shift = *optarg - 48;
+				}
+				break;
+			default:
+					printf("invalid command line\n");
+					return 0;
+		}
+	}
 	//...
 	return 0;
 }
