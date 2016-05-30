413f9418505e8187940f0314cddf9f155ee5ceaa
level55 <vitaliy.sherbakov58@gmail.com>
2016-05-30 19:49:51 +0700

    Small fix for if condition in caesar cipher

--- src/caesar.c
+++ src/caesar.c
@@ -17,20 +17,18 @@ char* Caesar(char* input_string)
 	printf("Input shift: ");
 	scanf("%d", &shift);
 	if (shift >= 26)
-		printf("Shift must be less than 26\n");
+		printf("Shift must be less than 26.\n");
 	} while (shift >= 26);
 	
 	for(i=0; str[i]!='\0'; i++)
 	{
 		if ((islower(str[i]) > 0) && (str[i] + shift > 'z'))
-		{
 			str[i] = 'a' + (shift - (('z' + 1) - str[i]));
-		}
 		else if ((isupper(str[i]) > 0) && (str[i] + shift > 'Z'))
 			str[i] = 'A' + (shift - (('Z' + 1) - str[i]));
 		else if (((str[i] >= 'a') && (str[i] < 'z')) || ((str[i] >= 'A') && (str[i] < 'Z')))
 			str[i] += shift;
 	}
-
+	
 	return str;
 }
